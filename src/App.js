dfd098f44b8af8b2cb36815330a7d3dbb399e019
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-05 18:56:22 +0430

    Home component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes , Route } from "react-router-dom";
 
 import SignUp from "./components/SignUp";
-import Login from "./components/Login"
+import Login from "./components/Login";
+import Home from './components/Home';
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/login' element={<Login />} />
       </Routes>
